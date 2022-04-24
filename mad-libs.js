e7048de1862fcc00b8b28328c292fbf2d919db80
Izi Martin <39589537+Izimartin@users.noreply.github.com>
2022-04-24 21:51:37 +0100

    ALX t ? Udacity

--- mad-libs.js
+++ mad-libs.js
@@ -16,4 +16,11 @@
  * 4. The madLib variable should be printed to the console
  */
  
- // your code goes here
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+
+// your code goes here
+
+var madLib = "The Intro to JavaScript course is " + adjective1 + ". James and Julia are so " + adjective2 + ". I cannot wait to work through the rest of this " + adjective3 +" content!";
+console.log(madLib);
