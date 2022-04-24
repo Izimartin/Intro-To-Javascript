621831e9ea52257fca63974ba6180c96ba9e838b
Izimartin <Izimartinz@gmail.com>
2022-04-24 17:34:51 +0000

    Alx T / Udacity

---
+++ first-expression.js
@@ -0,0 +1,16 @@
+/*
+ * Programming Quiz: First Expression
+ * Write an expression that uses at least three, distinct, arithmetic 
+ * operators to log the number 42 to the console.
+ */
+ 
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should print the value `42`
+ * 2. You should use at least 3 distinct operators.
+ *    (`+`, `-`, `*`, `/`, or `%`) 
+ */
+
+
+// this expression equals 4, change it to equal 42
+console.log(1 + 5 - 2 + 38);
