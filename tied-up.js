15ae9047a0ae7ff849bfe18a9eeb1f9a174128a3
Izi Martin <39589537+Izimartin@users.noreply.github.com>
2022-04-24 22:14:08 +0100

    ALX T / Udacity

--- tied-up.js
+++ tied-up.js
@@ -6,5 +6,8 @@
  * 2. Your joke should use only one string
  * 3. Your joke should match the expected format
 */
-const joke = /* write your joke here */
+
+// your code goes here
+
+const joke = "Why couldn't the shoes go out and play? \n They were all \"tied\" up!";
 console.log(joke);
